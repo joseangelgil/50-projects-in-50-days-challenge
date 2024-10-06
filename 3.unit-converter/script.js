4a67e9ec14198fc0146efc5f621fd3c4aa2b10ca
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2024-10-06 13:33:03 +0200

    Complete project#3
    Complete project#3: Unit Converter

--- 3.unit-converter/script.js
+++ 3.unit-converter/script.js
@@ -6,26 +6,106 @@ const inputEl = document.getElementById('input')
 const outputEl = document.getElementById('output')
 
 
-const units = [
-  'Tonne',
-  'Kilogram',
-  'Gram',
-  'Milligram',
-  'Microgram',
-  'Stone',
-  'Pound',
-  'Once'
-]
+const units = {
+  'mass': [
+    'Tonne',
+    'Kilogram',
+    'Gram',
+    'Milligram',
+    'Microgram',
+    'Stone',
+    'Pound',
+    'Once'
+  ],
+  'volume': [
+    'Liter',
+    'Milliliter',
+    'Centiliter',
+    'Cubic Meter',
+    'Pint'
+  ],
+  'length': [
+    'Meter',
+    'Kilometer',
+    'Centimeter',
+    'Millimeter',
+    'Micrometer',
+    'Nanometer',
+    'Mile',
+    'Yard',
+    'Foot',
+    'Inch',
+    'Nautical Mile'
+  ],
+  'time': [
+    'Day',
+    'Week',
+    'Month',
+    'Year',
+    'Decade',
+    'Century',
+    'Hour',
+    'Minute',
+    'Second'
+  ],
+  'speed': [
+    'Kilometer per Hour',
+    'Meter per Second',
+    'Mile per Hour',
+    'Foot per Second',
+    'Knot'
+  ]
+}
 
+const baseUnits = {
+  // base unit for mass = Gram
+    'Tonne': 0.000001,
+    'Kilogram': 0.001,
+    'Gram': 1,
+    'Milligram': 1000,
+    'Microgram': 1000000,
+    'Stone': 0.000157473,
+    'Pound': 0.00220462,
+    'Once': 0.035274,
 
-// create object with conversions table
-/* create an object with units
-units = {
-mass: []}
-and use units[typeDrodownEl.value].forEach...
-*/
+  // base unit for volume = Liter
+    'Liter': 1,
+    'Milliliter': 1000,
+    'Centiliter': 100,
+    'Cubic Meter': 0.001,
+    'Pint': 1.75978,
+    
+  // base unit for length = meter
+    'Meter': 1,
+    'Kilometer': 0.001,
+    'Centimeter': 100,
+    'Millimeter': 1000,
+    'Micrometer': 1000000,
+    'Nanometer': 1000000000,
+    'Mile': 0.000621371,
+    'Yard': 1.09361,
+    'Foot': 3.28084,
+    'Inch': 39.3701,
+    'Nautical Mile': 0.000539957,
 
-
+  // base unit for time = day
+    'Day': 1,
+    'Week': 0.142857,
+    'Month': 0.0328767,
+    'Year': 0.00273973,
+    'Decade': 0.000273973,
+    'Century': 0.0000273973,
+    'Hour': 24,
+    'Minute': 1440,
+    'Second': 86400,
+  
+  // base unit for speed = Meters per Second
+    'Kilometer per Hour': 3.6,
+    'Meter per Second': 1,
+    'Mile per Hour': 2.23694,
+    'Foot per Second': 3.28084,
+    'Knot': 1.94384
+}
 
 
 function createAndAppendOption(element, inputButton, outputButton){
@@ -40,200 +120,51 @@ function createAndAppendOption(element, inputButton, outputButton){
   outputButton.appendChild(createOption())
 }
 
-function imprime(){
+function createOptions(){
 
   inputBtn.innerHTML = '';
   outputBtn.innerHTML = '';
 
-  units.forEach(el => {
+  units[typeDropdownEl.value].forEach(el => {
     createAndAppendOption(el, inputBtn, outputBtn)
   })
 
+  convert(inputEl.value, inputBtn.value, outputBtn.value)
 }
 
-function convert() {
-  switch(inputBtn.value) {
-    case 'Tonne':
-      switch(outputBtn.value) {
-        case 'Tonne':
-          outputEl.innerText = inputEl.value;
-          break;
-        case 'Kilogram':
-          outputEl.innerText = (inputEl.value * 1000) || '';
-          break;
-        case 'Gram':
-          outputEl.innerText = (inputEl.value * 1000000) || '';
-          break;
-        case 'Milligram':
-          outputEl.innerText = (inputEl.value * 1000000000) || '';
-          break;
-        case 'Microgram':
-          outputEl.innerText = (inputEl.value * 1000000000000) || '';
-          break;
-        case 'Stone':
-          outputEl.innerText = (inputEl.value * 157.473) || '';
-          break;
-        case 'Pound':
-          outputEl.innerText = (inputEl.value * 2204.62) || '';
-          break;
-        case 'Once':
-          outputEl.innerText = (inputEl.value * 35274) || '';
-          break;
-        default:
-          outputEl.innerText = '';
-          break;
-      }
-      break;
-    case 'Kilogram':
-      switch(outputBtn.value) {
-        case 'Tonne':
-          outputEl.innerText = (inputEl.value / 1000) || '';
-          break;
-        case 'Kilogram':
-          outputEl.innerText = inputEl.value;
-          break;
-        case 'Gram':
-          outputEl.innerText = (inputEl.value * 1000) || '';
-          break;
-        case 'Milligram':
-          outputEl.innerText = (inputEl.value * 1000000) || '';
-          break;
-        case 'Microgram':
-          outputEl.innerText = (inputEl.value * 1000000000) || '';
-          break;
-        case 'Stone':
-          outputEl.innerText = (inputEl.value * 0.157473) || '';
-          break;
-        case 'Pound':
-          outputEl.innerText = (inputEl.value * 2.20462) || '';
-          break;
-        case 'Once':
-          outputEl.innerText = (inputEl.value * 35.274) || '';
-          break;
-        default:
-          outputEl.innerText = '';
-          break;
-      }
-    case 'Gram':
-      console.log('length');
-      break;
-    case 'Milligram':
-      console.log('volume');
-      break;
-    case 'Microgram':
-      console.log('length');
-      break;
-    case 'Stone':
-      console.log('time');
-      break;
-    case 'Pound':
-      console.log('speed');
-      break;
-    case 'Once':
-      console.log('speed');
-      break;
-    default:
-      alert('Please, select a type of unit to convert.')
-      break;
-  }
+function convert(value, fromUnit, toUnit) {
+  // Convert to baseUnit:
+  const baseValue = value / baseUnits[fromUnit]
+  // Then convert to expected unit from baseUnit:
+  const convertedValue = baseValue * baseUnits[toUnit];
 
-  if(outputEl.innerText.match(/\./)) outputEl.innerText = Number(outputEl.innerText).toFixed(2)
+  outputEl.innerText = convertedValue || '';
 
+  outputEl.innerText = outputEl.innerText.length <= 10 ? outputEl.innerText : Number(outputEl.innerText).toPrecision(4);
 }
 
-inputEl.addEventListener('keyup', convert)
 
+// LISTENERS
 
+inputEl.addEventListener('keyup', () => {
+  convert(inputEl.value, inputBtn.value, outputBtn.value)
+})
 
+inputEl.addEventListener('change', () => {
+  convert(inputEl.value, inputBtn.value, outputBtn.value)
+})
 
+inputBtn.addEventListener('change', () => {
+  convert(inputEl.value, inputBtn.value, outputBtn.value)
+})
 
+outputBtn.addEventListener('change', () => {
+  convert(inputEl.value, inputBtn.value, outputBtn.value)
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const units = [
-//   {
-//     Tonne,
-//     Kilogram,
-//     Gram,
-//     Milligram,
-//     Microgram,
-//     Stone,
-//     Pound,
-//     Once
-
-//   },
-//   {
-//     Liter,
-//     Milliliter,
-//     Centiliter,
-//     CubicMeter,
-//     Paint
-//   },
-//   {
-//     Meter,
-//     Kilometer,
-//     Centimeter,
-//     Millimeter,
-//     Micrometer,
-//     Nanometer,
-//     Mile,
-//     Yard,
-//     Foot,
-//     Inch,
-//     NauticalMile
-//   },
-//   {
-//     Day,
-//     Week,
-//     Month,
-//     Year,
-//     Decade,
-//     Century,
-//     Hour,
-//     Minute,
-//     Second
-//   },
-//   {
-//     KilometerPerHour,
-//     MeterPerSecond,
-//     MilePerHour,
-//     FootPerSecond,
-//     Knot
-//   }
-// ]
+changeBtn.addEventListener('click', () => {
+  const savedInput = inputBtn.value;
+  inputBtn.value = outputBtn.value;
+  outputBtn.value = savedInput;
+  convert(inputEl.value, inputBtn.value, outputBtn.value);
+})
