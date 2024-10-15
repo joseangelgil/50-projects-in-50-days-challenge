841c91290954ead2b1d023dc1c128c125a0c9b9a
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2024-10-15 17:20:39 +0200

    Fix project#12
    Fix lightbox. When clickin in an image after loading the page, lightbox image did not correspond to the image on the clicked image. When refresing the page, this problem did not happen. lightbox.src was removed from html to avoid initial randomness in its src. Also, all the images src were captured in an array after loading. This seems to have solve the problem.

--- 12.image-gallery-with-lightbox/script.js
+++ 12.image-gallery-with-lightbox/script.js
@@ -3,9 +3,14 @@ const images = document.querySelectorAll('.gallery-image')
 const lightboxImg = document.querySelector('.lightbox-image')
 const closeBtn = document.querySelector('.close-btn')
 
-images.forEach(img => {
+const imageSources = [];
+
+images.forEach((img, index) => {
+  imageSources[index] = img.src;
+
   img.addEventListener("click", () => {
-    lightboxImg.src = img.src
+    lightboxImg.src = imageSources[index]
+    lightboxImg.alt = img.alt
     lightbox.classList.add('active')
   })
 })
